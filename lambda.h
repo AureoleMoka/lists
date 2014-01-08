2500c7f31e28f365e826dd022f44eeb15dc559eb
AureoleMoka <aureolemoka@gmail.com>
2014-01-08 19:01:41 +0100

    Update lambda.h

--- lambda.h
+++ lambda.h
@@ -1,6 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// This code isn't written by me, i saw it on Superuser
+// this is awesome. It defines a function inline, by using
+// the lambda function. For instance:
+// lambda (int, (int a), {return a*2;}) defines the function:
+
+/*
+int anonymous_func (int a) {
+	return a*2;
+}
+*/
+
 #define lambda(ret_type, args, body) 	\
 	({									\
 		ret_type f args					\
